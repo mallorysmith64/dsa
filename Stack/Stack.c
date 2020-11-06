f5fe6f6ecdeb3d098b2c7f3375d1c4f81be4f9d8
mallorysmith64 <mallorysmith64@gmail.com>
2020-11-06 04:02:43 -0500

    logging everything pushed onto the stack in c now

--- Stack/Stack.c
+++ Stack/Stack.c
@@ -3,7 +3,14 @@
 int MAX_SIZE = 8;
 int stack[8];
 // when top = -1, stack is empty
+// top is same as size in the js file
 int top = -1;
+
+// return element at the top of stack without modifying
+int peek()
+{
+    return stack[top];
+}
 
 // cannot add if stack is full
 // not full then push element
@@ -21,8 +28,6 @@ int push(int data)
     {
         printf("Stack Overflow!!! \n");
     }
-
-    return data;
 }
 
 // cannot remove if empty
@@ -37,14 +42,17 @@ int pop(int data)
     return data;
 }
 
-int peek(int data)
-{
-    printf("peek element: %d \n", data);
-    return stack[top];
-}
-
+// test stack by performing operations
 int main()
 {
+    printf("Top of Stack is: %d\n", peek());
+
     push(3);
+    push(6);
+    push(9);
+    push(11);
+
     pop(9);
+
+    return 0;
 }
