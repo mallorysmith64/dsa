2ad975f51e40278863087df8a19e208f42de2710
mallorysmith64 <mallorysmith64@gmail.com>
2020-11-12 06:17:55 -0500

    improved stack pop method in c

--- Stack/Stack.c
+++ Stack/Stack.c
@@ -14,14 +14,14 @@ int peek()
 
 // cannot add if stack is full
 // not full then push element
-int push(int data)
+int push(int element)
 {
     // top < size - 1 makes sure stack isn't full
     if (top < MAX_SIZE - 1)
     {
         top++;
-        stack[top] = data;
-        printf("added element: %d \n", data);
+        stack[top] = element;
+        printf("added element: %d \n", element);
     }
 
     else
@@ -32,14 +32,17 @@ int push(int data)
 
 // cannot remove if empty
 // not empty then pop element
-int pop(int data)
+int pop(int element)
 {
 
     if (top >= 0)
-        stack[top] = data;
-    top--;
-    printf("removed element: %d \n", data);
-    return data;
+    {
+        stack[top] = element;
+        top--;
+        printf("removed element: %d \n", element);
+    }
+    else
+        printf("Stack is empty!!!");
 }
 
 // test stack by performing operations
