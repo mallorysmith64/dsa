cd4c69ec4a1d87cb13060112cb0fa6fa8f9a712b
mallorysmith64 <mallorysmith64@gmail.com>
2020-11-16 16:22:14 -0500

    printed singly linked list in c

--- SinglyLinked/singlyLinked.c
+++ SinglyLinked/singlyLinked.c
@@ -8,6 +8,14 @@ struct Node
     struct Node *next;
 };
 
+void printList(struct Node* n)
+{
+    while (n != NULL) {
+        printf("\n Value of Node:  %d", n-> data);
+        n = n-> next;
+    }
+}
+
 // create linked list
 int main()
 {
@@ -17,24 +25,27 @@ int main()
 
     // dynamically allocate the nodes in the heap
     // Data is random because we haven’t assigned anything yet
+    // if you printf variables, you will get values of 0
     head = (struct Node *)malloc(sizeof(struct Node));
     second = (struct Node *)malloc(sizeof(struct Node));
     third = (struct Node *)malloc(sizeof(struct Node));
 
     // assign data to first node
-    head->data = 1;
+    head->data = 100;
     // link first node to second
     head->next = second;
 
     // assign data to second
-    second->data = 2;
+    second->data = 25;
     // link second node to third
     second->next = third;
 
     // assign data to third
-    third->data = 3;
+    third->data = 30;
     // to terminate the list, last node points to null
     third->next = NULL;
+
+    printList(head);
 
     return 0;
 }
