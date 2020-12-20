2bce2384c7cf4d5bf3e63fc31763ef16524f60ab
mallorysmith64 <mallorysmith64@gmail.com>
2020-12-19 20:52:45 -0500

    found middle node in js

--- SinglyLinked/singlyLinked.js
+++ SinglyLinked/singlyLinked.js
@@ -5,24 +5,35 @@
 // link list methods include: clear(), size(), getFirst(), and getLast()
 
 // create node
-class LinkedListNode {
-  constructor(data) {
-    this.data = data;
-    // next node is unknown at first
-    this.next = null;
+class ListNode {
+  constructor(data, next = null) {
+   this.data = data;
+   this.next = next;
   }
 }
 
 // create linked list, head is the 1st node
-const head = new LinkedListNode(12);
+const head = new ListNode(12);
 // second or next node
-head.next = new LinkedListNode(99);
+head.next = new ListNode(99);
 // add third or next node after that
-head.next.next = new LinkedListNode(37);
+head.next.next = new ListNode(37);
+head.next.next.next = new ListNode(100);
+head.next.next.next.next = new ListNode(102);
 console.log(head);
 
 // So, we have 12 -> 99 -> 37 -> null
 
 // traverse/travel across the data in a linked list
-  let list = new LinkedListNode(head)
+  let list = new ListNode(head)
   console.log(`element is: ${list.data.next.data}`)
+
+// find middle node
+  let fast = head, slow = head;
+
+  while(fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+  }
+  console.log(`middle node is: ${slow.data}`);
+  return slow;
