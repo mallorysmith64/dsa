741e6c5c228971323f3d48978f14a870cee413c8
mallorysmith64 <mallorysmith64@gmail.com>
2020-12-26 19:43:50 -0500

    inserted into circular linked list in c

--- CircularLinked/circularLinked.c
+++ CircularLinked/circularLinked.c
@@ -47,7 +47,58 @@ void Rdisplay(struct node *p)
         printf("%d -> ", p->data);
         Rdisplay(p->next);
     }
-        flag = 0;
+    flag = 0;
+}
+
+// length of linked list
+int Length(struct node *p)
+{
+    int len = 0;
+
+    do
+    {
+        len++;
+        p = p->next;
+    } while (p != head);
+    return len;
+}
+
+void insert(struct node *p, int index, int x)
+{
+    struct node *t;
+    int i;
+
+    if (index < 0 || index > Length(head)) // check indices are valid
+        return;
+
+    if (index == 0) // check if index is 0
+    {
+        t = (struct node *)malloc(sizeof(struct node)); // create new node
+        t->data = x;                                    // assign data to new node
+
+        if (head == NULL) // check if head is NULL
+        {
+            head = t;          // if null, make head the new node
+            head->next = head; // there is only one node, make head point on itself
+        }
+        else
+        {
+            while (p->next != head) // transverse circular list
+                p = p->next;
+            p->next = t;    // when p is on the last node, p's next should point to new node
+            t->next = head; // new node's next should point to head
+            head = t;       // head is now the new node inserted
+        }
+    }
+    else
+    {
+        for (i = 0; i < index - 1; i++) // start at index 0 and insert node index - 1 times
+            p = p->next;
+        t = (struct node *)malloc(sizeof(struct node)); // create new node to be inserted
+        t->data = x;                                    // assign data to new node
+        t->next = p->next;
+        p->next = t; // p's next should point to new node
+    }
 }
 
 int main()
@@ -55,10 +106,12 @@ int main()
     int A[] = {2, 2, 2, 4, 8};
     create(A, 5);
 
-    Display(head);
-    
-    printf("\n");
+    insert(head, 2, 10); // insert at node, index, value
+
     Rdisplay(head);
 
     return 0;
 }
+
+// Display(head);
+// printf("\n");
